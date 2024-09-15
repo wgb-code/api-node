9f222fcf42cfe208ae26c04f1430dada3e7775d9
Willian <front.willian@gmail.com>
2024-09-15 16:33:20 -0300

    Instalando o PrismaORM

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-import { express, request } from 'express';
+import express from 'express';
+
+/* TODO: CRUD USUÁRIOS
+
+    1º - Criar usuários
+    2º - Listar usuários
+    3º - Ediar usuários
+    4º - Deletar usuários
+
+*/
 
 const app = express();
 
+app.use(express.json());
+
+let users = [];
+
+app.post('/users', (req, res) => {
+    users.push(req.body);
+
+    res.status(201).json(users);
+})
+
 app.get('/users', (request, response) => {
-    response.send('Resposta.');
+    response.status(200).json(users);
 });
 
 app.listen(3000);
